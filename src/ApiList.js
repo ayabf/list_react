4648475a2785dce20fb9f2d5288dd9c77a873c5b
ayabf <ayabenfraj7@gmail.com>
2023-12-26 22:11:49 +0100

    liste d'elements

---
+++ src/ApiList.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+const ApiList = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>List of elements from API:</h2>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ApiList;
